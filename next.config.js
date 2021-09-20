ce89e41b40e4c447d55f798c453931c65d7cea57
W.Songsak <songsak.want@gmail.com>
2021-09-20 11:39:57 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,5 +5,5 @@ module.exports = {
 
   // Use the basePath and prefix in production and not development.
   basePath: isProd ? '/nextjs-with-github-pages' : '',
-  assetPrefix: isProd ? '/nextjs-with-github-pages' : '',
+  assetPrefix: isProd ? '/nextjs-with-github-pages/_next/static/images/' : '',
 };

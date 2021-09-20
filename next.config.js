904e37c0d72070f93f8038e50f5fab2776af4cce
W.Songsak <songsak.want@gmail.com>
2021-09-20 10:53:32 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,4 +5,10 @@ module.exports = {
 
   // Use the prefix in production and not development.
   assetPrefix: isProd ? '/nextjs-with-github-pages/' : '',
+
+  // Image Optimization using Next.js' default loader is not compatible with `next export`.
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
 };

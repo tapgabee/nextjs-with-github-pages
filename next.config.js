3ef2f4e0c905d50c21dbd08fdfab90b456d4d2e9
W.Songsak <songsak.want@gmail.com>
2021-09-20 11:55:58 +0700

    update assets

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/nextjs-with-github-pages/_next/static/images/' : '',
+
+  assetPrefix: isProd
+    ? 'https://cdn.statically.io/gh/tapgabee/tapgabee.github.io/nextjs-with-github-pages/'
+    : '',
 };

814f0e00bca90b81161dea3bab6ea056067095e5
W.Songsak <songsak.want@gmail.com>
2021-09-20 10:48:18 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   reactStrictMode: true,
-}
+
+  // Use the prefix in production and not development.
+  assetPrefix: isProd ? '/nextjs-with-github-pages/' : '',
+};

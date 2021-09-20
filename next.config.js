32dc3d8c75728556acb3a825cd5d36d2e9cfb654
W.Songsak <songsak.want@gmail.com>
2021-09-20 11:42:26 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-
-  // Use the basePath and prefix in production and not development.
-  basePath: isProd ? '/nextjs-with-github-pages' : '',
   assetPrefix: isProd ? '/nextjs-with-github-pages/_next/static/images/' : '',
 };
